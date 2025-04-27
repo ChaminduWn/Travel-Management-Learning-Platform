db4439838039dd374d72e2ba7169853c9aa4109c
ChaminduWN <127916715+chamindu2001@users.noreply.github.com>
2025-04-28 02:40:47 +0530

    create user DTO java file and according to that reolved the error chat ad message dto

---
+++ server/tranvelmanagement/src/main/java/com/tranvelmanagement/tranvelmanagement/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.tranvelmanagement.tranvelmanagement.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String pic;
+}
