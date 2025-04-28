3c1b3d92ae54474309e717123ba14df21556a86f
ChaminduWN <127916715+chamindu2001@users.noreply.github.com>
2025-04-28 23:53:13 +0530

    modify footer,home ,header,login and profile

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -76,9 +76,9 @@ const Register = () => {
           <form onSubmit={handleSubmit} className="space-y-4">
             <div className="space-y-2">
               <label htmlFor="username" className="flex items-center text-sm font-medium text-gray-300">
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                </svg>
+                </svg> */}
                 Username
               </label>
               <input
@@ -94,9 +94,9 @@ const Register = () => {
             
             <div className="space-y-2">
               <label htmlFor="email" className="flex items-center text-sm font-medium text-gray-300">
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                </svg>
+                </svg> */}
                 Email
               </label>
               <input
@@ -112,9 +112,9 @@ const Register = () => {
             
             <div className="space-y-2">
               <label htmlFor="password" className="flex items-center text-sm font-medium text-gray-300">
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-                </svg>
+                </svg> */}
                 Password
               </label>
               <input
@@ -130,9 +130,9 @@ const Register = () => {
             
             <div className="space-y-2">
               <label htmlFor="confirmPassword" className="flex items-center text-sm font-medium text-gray-300">
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
+                </svg> */}
                 Confirm Password
               </label>
               <input
